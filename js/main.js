34c9af98d5add7899426f14b6cd6f9330e52b033
Nathalia Roberts <31518187+NJRoberts@users.noreply.github.com>
2018-03-25 08:47:48 -0700

    Update

--- js/main.js
+++ js/main.js
@@ -1,184 +1,84 @@
 //execute script when window is loaded
-window.onload = function(){
+window.onload = setMap();
 
-//SVG Dimension variables
-var w = 900, h = 500;
+//set up choropleth map
+  function setMap(){
+      //map frame dimensions
+      var width = 960,
+          height = 460;
 
-var container = d3.select("body") //get the <body> element from the DOM
-    .append("svg") // put a new svg in the body
-    .attr("width", w) //assign the width
-    .attr("height", h) //assign the height
-    .attr("class", "container") // always assign a class (as the block name) for styling and future selection
-    .style("background-color", "rgba(0,0,0,0.2)"); //only put a semicolon at the end of the block!
+      //create new svg container for the map
+      var map = d3.select('body')
+          .append('svg')
+          .attr('class', 'map')
+          .attr('width', width)
+          .attr('height', height);
+
+      //create California Albers conic equal area projection
+      var projection = d3.geoConicEqualArea()
+          .center([4.20, 37.40])
+          .parallels([34, 40.5])
+          .rotate([120, 0])
+          .scale(2600)
+          .translate([width / 2, height / 2]);
+
+      var path = d3.geoPath()
+          .projection(projection);
 
 
-var innerRect = container.append("rect") //put in a new rect in the SVG
-    .datum(400) // a single value is a datum
-    .attr("width", function(d){ //rectangle width
-                return d * 2; //400 * 2 = 800
+      //use d3.queue to parallelize asynchronous data loading
+      d3.queue()
+          .defer(d3.csv, 'data/CA_hist_pop.csv') //load attributes from csv
+          //.defer(d3.json, ) //load background spatial data
+          .defer(d3.json, 'data/county_CA.topojson') //load choropleth spatial data
+          .await(callback);
+
+
+      function callback(error, csvData, ca){
+        // translate CA TopoJSON
+        var countiesCA = topojson.feature(ca, ca.objects.county_CA).features;
+          //console.log(error);
+          //console.log(csvData); popDen data
+          //console.log('here be countiesCA');
+          //console.log(countiesCA);
+
+
+          /////Graticule below - optional for project
+
+//create graticule generator ..drawing order matters
+        var graticule = d3.geoGraticule()
+        .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
+
+        //create graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline()) //bind graticule background
+            .attr("class", "gratBackground") //assign class for styling
+            .attr("d", path) //project graticule
+
+
+        //create graticule lines
+        var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
+
+      /////Graticule above
+
+
+
+
+        var counties = map.selectAll('.counties')// create an empty selection
+            .data(countiesCA)
+            .enter()
+            .append('path')
+            .attr('class', function(d) {
+              return 'County ' + d.properties.NAME
             })
-    .attr("height", function(d){ //rectangle height
-                return d; //400
-            })
-    .attr("class", "innerRect") //class name
-    .attr("x", 50) //position from left on the x (horizontal) axis
-    .attr("y", 50) //position from top on the y (vertical) axis
-    .style("fill", "#FFFFFF"); //fill color
-
-
-    var cityPop = [
-        {
-            city: 'Madison',
-            population: 233209
-        },
-        {
-            city: 'Milwaukee',
-            population: 594833
-        },
-        {
-            city: 'Green Bay',
-            population: 104057
-        },
-        {
-            city: 'Superior',
-            population: 27244
-        },
-        {
-            city: 'Oakland',
-            population: 420005
-        },
-        {
-            city: 'Minneapolis',
-            population: 413651
-        }
-    ];
-
-
-    //scale must be above circles block
-    var x = d3.scaleLinear()  //create the scale
-        .range([90, 810]) //output min and max
-        .domain([0, 3]); //input min and max
-
-
-      //find the minimum value of the array
-    var minPop = d3.min(cityPop, function(d){
-          return d.population;
-    });
-
-    //find the maximum value of the array
-    var maxPop = d3.max(cityPop, function(d){
-        return d.population;
-    });
-
-    //scale for circles center y coordinate
-    var y = d3.scaleLinear()
-        .range([450, 50]) // was 440, 95
-        .domain([0, 700000]); // was minPop, maxPop
-
-
-//color scale generator
-    var color = d3.scaleLinear()
-       .range([
-           "#FDBE85",
-           "#D94701"
-       ])
-       .domain([
-           minPop,
-           maxPop
-     ]);
+            .attr('d', path);
 
 
 
-
-
-    //Example 2.6 line 3
-    var circles = container.selectAll(".circles") //create an empty selection
-        .data(cityPop) //here we feed in an array
-        .enter() //one of the great mysteries of the universe
-        .append("circle") //inspect the HTML--holy crap, there's some circles there
-        .attr("class", "circles")
-        .attr("id", function(d){
-            return d.city;
-        })
-        .attr("r", function(d){
-            //calculate the radius based on population value as circle area
-            var area = d.population * 0.01;
-            return Math.sqrt(area/Math.PI);
-        })
-        .attr("cx", function(d, i){
-            //use the index to place each circle horizontally
-            return x(i);
-        })
-        .attr("cy", function(d){
-            return y(d.population);
-        })
-        .style("fill", function(d, i){ //add a fill based on the color scale generator
-            return color(d.population);
-         })
-        .style("stroke", "#000"); //black circle stroke
-
-
-
-    //Example 3.6 line 1...create y axis generator
-    var yAxis = d3.axisLeft(y)
-       .scale(y);
-
-    //create axis g element and add axis
-    var axis = container.append("g")
-       .attr("class", "axis")
-       .call(yAxis)
-       .attr("transform", "translate(50, 0)")
-       .call(yAxis);
-
-//below Example 3.9...create a text element and add the title
-   var title = container.append("text")
-       .attr("class", "title")
-       .attr("text-anchor", "middle")
-       .attr("x", 450)
-       .attr("y", 30)
-       .text("City Populations");
-
-//Example 3.14 line 1...create circle labels
-  var labels = container.selectAll(".labels")
-      .data(cityPop)
-      .enter()
-      .append("text")
-      .attr("class", "labels")
-      .attr("text-anchor", "left")
-      .attr("y", function(d){
-          //vertical position centered on each circle
-          return y(d.population) + 5;
-      });
-
-  //first line of label
-  var nameLine = labels.append("tspan")
-      .attr("class", "nameLine")
-      .attr("x", function(d,i){
-          //horizontal position to the right of each circle
-          return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-      })
-      .text(function(d){
-          return d.city;
-      });
-
-  var format = d3.format(",");
-
-  //second line of label
-  var popLine = labels.append("tspan")
-      .attr("class", "popLine")
-      .attr("x", function(d,i){
-          //horizontal position to the right of each circle
-          return x(i) + Math.sqrt(d.population * 0.01 / Math.PI) + 5;
-      })
-      .attr("dy", "15") //vertical offset
-      .text(function(d){
-           return "Pop. " + format(d.population); //use format generator to format numbers
-      });
-
-
-
-
-
-
-
-};
+      };
+  };
