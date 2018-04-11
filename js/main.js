e082dd7da24a16328dc8b71faa9775d48ce6f8b8
Nathalia Roberts <31518187+NJRoberts@users.noreply.github.com>
2018-04-11 08:22:01 -0700

    final changes before submit

--- js/main.js
+++ js/main.js
@@ -1,91 +1,106 @@
-(function(){
+//File for d3 lab in GEOG 575. Nathalia Roberts, Spring 2018
 
+(function(){
       //pseudo-global variables
-      var attrArray = ["d1860","d1890","d1920","d1950","d1980","d2010"];
+      var attrArray = ["d1860","d1860SF","d1890","d1890SF","d1920","d1920SF",
+                       "d1950","d1950SF","d1980","d1980SF","d2010","d2010SF"
+                      ];
       var expressed = attrArray[0]; //initial attribute
 
       //chart frame dimensions
       var chartWidth = window.innerWidth * 0.425,
-          chartHeight = 473,
-          leftPadding = 25,
+          chartHeight = 570, // was 473
+          leftPadding = 31,
           rightPadding = 2,
           topBottomPadding = 5,
           chartInnerWidth = chartWidth - leftPadding - rightPadding,
           chartInnerHeight = chartHeight - topBottomPadding * 2,
           translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
 
+      // attribute names for attrArray Years
+      var d1860 = '1860';
+      var d1890 = '1890';
+      var d1920 = '1920';
+      var d1950 = '1950';
+      var d1980 = '1980';
+      var d2010 = '2010';
+      // attribute names for attrArray SF(excluded)
+      var d1860SF = '1860 - SF Excluded';
+      var d1890SF = '1890 - SF Excluded';
+      var d1920SF = '1920 - SF Excluded';
+      var d1950SF = '1950 - SF Excluded';
+      var d1980SF = '1980 - SF Excluded';
+      var d2010SF = '2010 - SF Excluded';
+
+      var attrName = {
+         d1860 : '1860',
+         d1860SF: '1860',
+         d1890 : '1890',
+         d1890SF: '1890',
+         d1920 : '1920',
+         d1920SF: '1920',
+         d1950 : '1950',
+         d1950SF: '1950',
+         d1980 : '1980',
+         d1980SF: '1980',
+         d2010 : '2010',
+         d2010SF : '2010'
+      }
+
       //create a scale to size bars proportionally to frame
       var yScale = d3.scaleLinear()
-           .range([463, 0]) //bar height - adjust data to improve presentation
-           .domain([0, 100]);
+           .range([560, 0])
+           .domain([0, 50]);
 
+        //execute script when window is loaded
+        window.onload = setMap();
 
+  //set up choropleth map
+    function setMap(){
+        //map frame dimensions
+        var width = window.innerWidth * .5,
+            height = 560;
 
-//execute script when window is loaded
-window.onload = setMap();
-
-//set up choropleth map
-  function setMap(){
-      //map frame dimensions
-      var width = window.innerWidth * 0.5,
-          height = 460;
-
-      //create new svg container for the map
-      var map = d3.select('body')
-          .append('svg')
-          .attr('class', 'map')
-          .attr('width', width)
-          .attr('height', height);
+        //create new svg container for the map
+        var map = d3.select("#mapContainer")
+            .append('svg')
+            .attr('class', 'map')
+            .attr('width', width)
+            .attr('height', height);
 
       //create California Albers conic equal area projection
       var projection = d3.geoConicEqualArea()
           .center([4.20, 37.40])
           .parallels([34, 40.5])
           .rotate([120, 0])
-          .scale(2600)
+          .scale(3200)
           .translate([width / 2, height / 2]);
 
       var path = d3.geoPath()
           .projection(projection);
 
-
       //use d3.queue to parallelize asynchronous data loading
       d3.queue()
-          .defer(d3.csv, 'data/CA_hist_pop.csv') //load attributes from csv
+          .defer(d3.csv, 'data/CA_hist_pop_v2.csv') //load attributes from csv
           //.defer(d3.json, ) //load background spatial data
           .defer(d3.json, 'data/county_CA.topojson') //load choropleth spatial data
           .await(callback);
 
-
       function callback(error, csvData, ca){
-
-        //place graticule on the map --optional uncomment function to use
-        setGraticule(map, path);
 
         // translate CA TopoJSON
         var countiesCA = topojson.feature(ca, ca.objects.county_CA).features;
-          //console.log(error);
-          //console.log(csvData); //popDen data
-          //console.log('here be countiesCA');
-          //console.log(countiesCA);
-
 
             //join csv data to GeoJSON enumeration units
             countiesCA = joinData(countiesCA, csvData);
-
             //create the color scale
             var colorScale = makeColorScale(csvData);
-
             //add enumeration units to the map
             setEnumerationUnits(countiesCA, map, path, colorScale);
-
             //add coordinated visualization to the map
             setChart(csvData, colorScale);
-
             //add dropdown
             createDropdown(csvData, colorScale);
-
-
 
       };
   }; //end of setMap
@@ -94,7 +109,7 @@ window.onload = setMap();
   function setChart(csvData, colorScale){
 
       //create a second svg element to hold the bar chart
-      var chart = d3.select("body")
+      var chart = d3.select("#mapContainer")
           .append("svg")
           .attr("width", chartWidth)
           .attr("height", chartHeight)
@@ -116,7 +131,7 @@ window.onload = setMap();
              return b[expressed]-a[expressed] //reversed for biggest to small..
           })
           .attr("class", function(d){
-              return "bar " + d.NAME // not sure correct method (bar? /GEOID?/ County?)
+              return "bar " + d.NAME //change to NAMELSAD?
           })
           .attr("width", chartInnerWidth / csvData.length - 1)
           .on("mouseover", highlight)
@@ -129,13 +144,14 @@ window.onload = setMap();
 
        //create a text element for the chart title
        var chartTitle = chart.append("text")
-           .attr("x", 40)
+           .attr("x", 90)
            .attr("y", 40)
            .attr("class", "chartTitle")
 
       //create vertical axis generator
       var yAxis = d3.axisLeft()
-          .scale(yScale)
+          .scale(yScale) //yScale
+
 
       //place axis
       var axis = chart.append("g")
@@ -154,7 +170,6 @@ window.onload = setMap();
 
   }; //end of setChart()
 
-
   //function to position, size, and color bars in chart
   function updateChart(bars, n, colorScale){
         //position bars
@@ -163,7 +178,7 @@ window.onload = setMap();
         })
         //size/resize bars
         .attr("height", function(d, i){
-            return 463 - yScale(parseFloat(d[expressed]));
+            return 560 - yScale(parseFloat(d[expressed]));
         })
         .attr("y", function(d, i){
             return yScale(parseFloat(d[expressed])) + topBottomPadding;
@@ -173,22 +188,16 @@ window.onload = setMap();
             return choropleth(d, colorScale);
         });
 
-       //at the bottom of updateChart()...add text to chart title
-       // expressed can be index (see examples)
        var chartTitle = d3.select(".chartTitle")
-          .text("Number of Variable " + expressed + " in each county");
+          .text("Population Density in " + attrName[expressed] + " by County");
   }; //end of updateChart
-
 
 
   //function to create dynamic label
   function setLabel(props){
       //label content
-      var labelAttribute = "<h1>" + props.NAME +
-          "</h1><b>" + expressed + "</b>";
-
-      // original: var labelAttribute = "<h1>" + props[expressed] +
-      // change to NAMESLAD when csv also has the column
+      var labelAttribute = "<h1>" + props.NAMELSAD +
+      "</h1><br>" + "People per mile: " + props[expressed] + " in " + attrName[expressed] + "</br>";
 
       //create info label div
       var infolabel = d3.select("body")
@@ -196,7 +205,6 @@ window.onload = setMap();
           .attr("class", "infolabel")
           .attr("id", props.NAME + "_label")
           .html(labelAttribute);
-
 
       var countyName = infolabel.append("div")
           .attr("class", "labelname")
@@ -227,7 +235,6 @@ window.onload = setMap();
           .style("top", y + "px");
   };
 
-
   //function to highlight enumeration units and bars
   function highlight(props){
      //change stroke
@@ -238,7 +245,6 @@ window.onload = setMap();
          //call setLabel function
          setLabel(props);
   };
-
 
   //function to reset the element style on mouseout
  function dehighlight(props){
@@ -258,7 +264,7 @@ window.onload = setMap();
 
          var styleObject = JSON.parse(styleText);
 
-         //below Example 2.4 line 21...remove info label
+         //remove info label
         d3.select(".infolabel")
         .remove();
 
@@ -267,33 +273,6 @@ window.onload = setMap();
      };
 
  };
-
-
-
-
-  //Graticule below - optional for project
-  function setGraticule(map, path){
-
-  //create graticule generator ..drawing order matters
-  var graticule = d3.geoGraticule()
-  .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
-
-  //create graticule background
-  var gratBackground = map.append("path")
-      .datum(graticule.outline()) //bind graticule background
-      .attr("class", "gratBackground") //assign class for styling
-      .attr("d", path) //project graticule
-
-  //create graticule lines
-  var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
-      .data(graticule.lines()) //bind graticule lines to each element to be created
-      .enter() //create an element for each datum
-      .append("path") //append each element to the svg as a path element
-      .attr("class", "gratLines") //assign class for styling
-      .attr("d", path); //project graticule lines
-
-  };
-
 
   function joinData(countiesCA, csvData){
     //...DATA JOIN LOOPS
@@ -307,9 +286,7 @@ window.onload = setMap();
 
         var geojsonProps = countiesCA[a].properties; //the current coutnty geojson properties
         var geojsonKey = geojsonProps.GEOID; //the geojson primary key
-        //console.log(geojsonProps);
-        //console.log(geojsonKey);
-        //console.log(csvCounty);
+
         //where primary keys match, transfer csv data to geojson properties object
         if (geojsonKey == csvKey) {
 
@@ -317,7 +294,6 @@ window.onload = setMap();
          attrArray.forEach(function(attr){
            var val = parseFloat(csvCounty[attr]); //get csv attribute values
            geojsonProps[attr] = val; //assign attribute and value to geojson properties
-
 
          });
         }
@@ -329,19 +305,18 @@ window.onload = setMap();
     return countiesCA;
 }; //end joinData()
 
-
 function setEnumerationUnits(countiesCA, map, path, colorScale){
-    //...REGIONS BLOCK FROM PREVIOUS MODULE
+
     var counties = map.selectAll('.counties')// create an empty selection
         .data(countiesCA)
         .enter()
         .append('path')
         .attr('class', function(d) {
-          return 'County ' + d.properties.NAME; //examples points to adm1_code..not NAME but it works
+          return 'County ' + d.properties.NAME;
         })
         .attr('d', path)
         .style("fill", function(d){
-            return choropleth(d.properties, colorScale); //this changed
+            return choropleth(d.properties, colorScale);
         })
         .on("mouseover", function(d){
             highlight(d.properties);
@@ -356,16 +331,17 @@ function setEnumerationUnits(countiesCA, map, path, colorScale){
           .text('{"stroke": "#222", "stroke-width": "0.25px"}');
 };
 
-
 //function to create color scale generator
 function makeColorScale(data){
 // Quantile color scale. Check that this scale works for Natural Breaks
     var colorClasses = [
-        "#D4B9DA",
-        "#C994C7",
-        "#DF65B0",
-        "#DD1C77",
-        "#980043"
+      "#ece7f2",
+    	"#d0d1e6",
+    	"#a6bddb",
+    	"#74a9cf",
+    	"#3690c0",
+    	"#0570b0",
+    	"#034e7b"
     ];
 
     //create color scale generator
@@ -380,7 +356,7 @@ function makeColorScale(data){
     };
 
     //cluster data using ckmeans clustering algorithm to create natural breaks
-    var clusters = ss.ckmeans(domainArray, 6);
+    var clusters = ss.ckmeans(domainArray, 7);
     //reset domain array to cluster minimums
     domainArray = clusters.map(function(d){
         return d3.min(d);
@@ -407,7 +383,6 @@ function choropleth(props, colorScale){
     };
 };
 
-
 //function to create a dropdown menu for attribute selection
 function createDropdown(csvData){
     //add select element
@@ -430,14 +405,85 @@ function createDropdown(csvData){
         .enter()
         .append("option")
         .attr("value", function(d){ return d })
-        .text(function(d){ return d });
+        .text(function(d){
+                if (d == attrArray[0]) {
+            d = d1860;
+          } else if (d == attrArray[1]) {
+            d = d1860SF;
+          } else if (d == attrArray[2]) {
+            d = d1890;
+          } else if (d == attrArray[3]) {
+            d = d1890SF;
+          } else if (d == attrArray[4]) {
+            d = d1920;
+          } else if (d == attrArray[5]) {
+            d = d1920SF;
+          } else if (d == attrArray[6]) {
+            d = d1950;
+          } else if (d == attrArray[7]) {
+            d = d1950SF;
+          } else if (d == attrArray[8]) {
+            d = d1980;
+          } else if (d == attrArray[9]) {
+            d = d1980SF;
+          } else if (d == attrArray[10]) {
+            d = d2010;
+          } else if (d == attrArray[11]) {
+            d = d2010SF;
+          };
+
+          return d
+        });
 };
 
 //dropdown change listener handler
 function changeAttribute(attribute, csvData){
     //change the expressed attribute
     expressed = attribute;
-    //console.log(attribute);
+
+// Get the max value for the selected attribute
+ var max = d3.max(csvData,function(d){
+     return +d[expressed];});
+
+ ;
+
+// yScale is a global variable - just set the domain to 0 and the max value you found. Adjust if needed.
+        if (expressed == attrArray[1]){
+         yScale = d3.scaleLinear()
+            .range([560, 0])
+            .domain([0, 50])
+            .nice();
+     } else if (expressed == attrArray[3]){
+      yScale = d3.scaleLinear()
+         .range([560, 0])
+         .domain([0, 200])
+         .nice();
+     } else if (expressed == attrArray[5]){
+         yScale = d3.scalePow()
+             .range([560,0])
+             .domain([0,500])
+             .nice();
+     } else if (expressed == attrArray[7]){
+         yScale = d3.scaleLinear()
+             .range([560,0])
+             .domain([0,1400])
+             .nice();
+     } else if (expressed == attrArray[9]){
+         yScale = d3.scaleLinear()
+             .range([560,0])
+             .domain([0,2500])
+             .nice();
+     } else if (expressed == attrArray[11]){
+         yScale = d3.scaleLinear()
+             .range([560,0])
+             .domain([0,5000])
+             .nice();
+     }  else {
+         yScale = d3.scaleLinear()
+             .range([560,0])
+             .domain([0,max])
+             .nice();
+     }
 
     //recreate the color scale
     var colorScale = makeColorScale(csvData);
@@ -477,9 +523,29 @@ function changeAttribute(attribute, csvData){
         });
 
         updateChart(bars, csvData.length, colorScale);
+
+      //copied from setChart, don't delete the original
+        var yAxis = d3.axisLeft()
+            .scale(yScale)
+             //display short abbreviations for large numbers
+            .tickFormat(function (d) {
+        var array = ['','K','M','G','T','P'];
+        var i=0;
+        while (d >= 2000)
+        {
+            i++;
+            d = d/1000;
+        }
+
+        d = d + array[i];
+
+        return d;})
+
+        //update the charts axis
+        d3.selectAll("g.axis")
+        .call(yAxis);
+
 }; // end of changeAttribute()
-
-
 
 
 })(); //last line of main.js
